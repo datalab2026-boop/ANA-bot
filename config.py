bc45b84fdbedd53fefe58030716e11469f8a5e4d
Nightmare <datalab2026@gmail.com>
2026-03-27 20:43:57 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+DISCORD_TOKEN = os.environ.get("Bottoken")
+ROBLOX_API_KEY = os.environ.get("Apitoken")
+RESTART_TOKEN = os.environ.get("Restarttoken")
+
+GROUP_ID = 841435331
+ALLOWED_ROLES = [] 
+ACTIONS_LOG = 0
+BUG_LOGS = 0
+
+RANKS = {}       
+VALID_RANKS = [] 
+MAX_RANK_THRESHOLD = 200 # Установите число (0-255), ниже которого ранги считаются VALID
