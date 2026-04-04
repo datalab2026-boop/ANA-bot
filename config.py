57fb8f0455458b6becb7b148e20adc713b9f0e02
Nightmare <datalab2026@gmail.com>
2026-04-04 04:08:43 +0300

    Add new configuration variables
    Added new environment variables for RESTART_TOKEN, ROVER_API, and MONGO_URL.

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+# important keys
+BOT_TOKEN = os.environ.get("BotToken")
+CLOUD_API = os.environ.get("CloudAPI")
+RESTART_TOKEN = os.environ.get("RestartToken")
+ROVER_API = os.environ.get("RoverAPI")
+MONGO_URL = os.environ.get("MONGO_URL")
+# Maintenance
+ERROR_CHANNEL =
+LOGS_CHANNEL =
